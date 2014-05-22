fa46b72fccda2465a93653206e59457212f0c5ee
asholok <asholok@gmail.com>
2014-05-22 16:05:10 +0300

    make more security and less preformance (get_id_list ==> get_all)

--- DBworker.py
+++ DBworker.py
@@ -10,37 +10,45 @@ class DBworker(object):
 		self._connection = psycopg2.connect(self._conn_string)
 		self._cursor = self._connection.cursor()
 
-	def __get_last_id(self, table):
-		self._cursor.execute('SELECT MAX({0}_id) FROM "{0}"'.format(table),)
-
-		return self._cursor.fetchone()[0]
-
 	def select(self, table, row_id):
-		self._cursor.execute('SELECT * FROM "{0}" WHERE {0}_id = {1}'.format(table, row_id),)
-		row = self._cursor.fetchone()
-		fields = [info[0] for info in self._cursor.description]
+			self._cursor.execute('SELECT * FROM "{0}" WHERE {0}_id = {1}'.format(table, row_id),)
+			row = self._cursor.fetchone()
+			fields = [info[0] for info in self._cursor.description]
 
-		return dict(zip(fields, row))
+			return dict(zip(fields, row))
 	
-	def insert(self, table, fields, values):
-		self._cursor.execute('INSERT INTO "{}" ({}) VALUES ({})'.format(table, fields, values),)
-		self._connection.commit()
+	def insert(self, table, fields, holders, values):
+		query = 'INSERT INTO "{0}" ({1}) VALUES ({2}) RETURNING "{0}_id"'.format(table, fields, holders)
 
-		return self.__get_last_id(table)
+		try:
+			self._cursor.execute(query, values)
+			self._connection.commit()
 
-	def update(self, table, fields_values, row_id):
-		self._cursor.execute('UPDATE "{0}" SET {1} WHERE {0}_id = {2}'.format(table, fields_values, row_id),)
-		self._connection.commit()
+			return self._cursor.fetchone()[0]
+		except:
+			self._connection.rollback()
+
+	def update(self, table, fields, row_id, values):
+		query = 'UPDATE "{0}" SET {1} WHERE {0}_id = {2}'.format(table, fields, row_id)
+
+		try:
+			self._cursor.execute(query, values)
+			self._connection.commit()
+		except:
+			self._connection.rollback()
 
 	def delete(self, table, row_id):
-		self._cursor.execute('DELETE FROM "{0}" WHERE {0}_id = {1}'.format(table, row_id),)
-		self._connection.commit()
+		try:
+			self._cursor.execute('DELETE FROM "{0}" WHERE {0}_id = {1}'.format(table, row_id),)
+			self._connection.commit()
+		except:
+			self._connection.rollback()
 
-	def get_id_list(self, table):
-		self._cursor.execute('SELECT {0}_id FROM "{0}"'.format(table),)
-		id_list = [row[0] for row in self._cursor.fetchall()]
+	def get_all(self, table):
+		self._cursor.execute('SELECT * FROM "{0}"'.format(table),)
+		fields = [info[0] for info in self._cursor.description]
 
-		return id_list
+		return [dict(zip(fields, row)) for row in self._cursor.fetchall()]
 
 	def change_connection(self, new_conn_string):
 		self._conn_string = new_conn_string
--- Entity.py
+++ Entity.py
@@ -5,7 +5,7 @@ def get_class_name(cls):
 	return cls.__class__.__name__.lower()
 
 class Entity(object):
-	__loaded = False
+	_loaded = False
 	_fields 		= []
 	_managed_fields = {}
 	_row_dict 	= {}
@@ -16,43 +16,53 @@ class Entity(object):
 		self._db_worker = DBworker()
 
 	def load(self):
-		if self._row_id is None or self.__loaded:
+		if self._row_id is None or self._loaded:
 			return
-		self._row_dict = self._db_worker.select(self._table_name, self._row_id)
-		self.__loaded = True
+		self._row_dict = dict(self._db_worker.select(self._table_name, self._row_id))
+		self._loaded = True
 
 	def __managin_input(self, name, value):
 		field_name = '{}_{}'.format(self._table_name, name)
 		
-		self._managed_fields[field_name] = "'"+re.escape(value)+"'"
+		self._managed_fields[field_name] = re.escape(value)
 
 	def __make_update_conent(slef):
-		result = ['{} = {}'.format(key, value) for key, value in self._managed_fields.items()]
+		fields = []
+		values = []
 
-		return ", ".join(result)
+		for key, value in self._managed_fields.items():
+			result.append('{} = %s'.format(key))
+			values.append(value)
+		
+		return {"fields": ', '.join(fields), 'values': values}
 
 	def __make_insert_conent(self):
 		fields = []
+		holders = []
 		values = []
 		
 		for key, value in self._managed_fields.items():
 			fields.append(key)
+			holders.append('%s')
 			values.append(value)
 
-		return {"fields": ', '.join(fields), "values": ', '.join(values)}
+		return {"fields": ', '.join(fields), "holders": ', '.join(holders), 'values': values}
 
 	def save(self):
 		if not self._managed_fields:
 			return
 		
 		if self._row_id:
-			self._db_worker.update(self._table_name, self.__make_update_conent(), self._row_id)
+			content = self.__make_update_conent()
+
+			self._db_worker.update(self._table_name, content["fields"], self._row_id, content["values"])
 		else:
 			content = self.__make_insert_conent()
 			
-			self._row_id = self._db_worker.insert(self._table_name, content["fields"], content["values"])
+			self._row_id = self._db_worker.insert(self._table_name, content["fields"], 
+												  content["holders"], content["values"])
 
-		self.__loaded = False
+		self._loaded = False
 		self._managed_fields = []
 
 	def delete(self):
@@ -84,8 +94,17 @@ class Entity(object):
 		object.__setattr__(self, name, value)
 	
 	@classmethod
-	def all(cls):
-		semple = DBworker()
-		entitys_list = [cls(i) for i in semple.get_id_list(get_class_name(cls))]
+	def enforce_clas(cls, stack):
+		semple = cls()
 
-		return entitys_list
+		semple._loaded = True
+		semple._row_id = stack['{}_id'.format(semple._table_name)]
+		semple._row_dict = stack
+
+		return semple
+
+	@classmethod
+	def all(cls):
+		worker = DBworker()
+
+		return [cls.enforce_clas(stack) for stack in worker.get_all(cls.__name__.lower())]
